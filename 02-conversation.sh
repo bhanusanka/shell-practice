7cc69757e0c130d90e264d800282fba61cd90f75
Sanka Bhanu <bhanusanka3@gmail.com>
2025-07-12 12:42:51 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,4 +2,4 @@
 
 echo "bhanu:Hi how are you sanka"
 echo "sanka:Good what abou you"
-echo "bhanu:good bhanu"
+echo "bhanu:good sanka"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/Bash
+
+person1=bhanu
+person2=sanka
+
+echo "$person1:Hi how are you $person2"
+echo "$person2:Good what abou you"
+echo "$person1:good $person2"
